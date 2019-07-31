f11377283900cdb17f4384db88aa849c28505aca
Boolean <x13352840347@gmail.com>
2019-07-31 14:56:02 +0800

    修复固定高度 数据为空，底部边框消失的bug

--- App.vue
+++ App.vue
@@ -17,55 +17,95 @@
 	@import "./static/flex.less";
 </style>
 <style lang="scss">
+	.no-bad-table-wrap {
+		.rowClassName {
+			.td {
+				background: red;
 
-	.rowClassName {
-		.td {
-			background: red;
+				.td_wrap {
+					background: red;
+					color: #fff;
+				}
+			}
+		}
+
+		//自定义某一行样式
+		.demo-table-info-row .td {
+			background-color: #2db7f5;
+			color: #fff;
+
+			.td_wrap {
+				background-color: #2db7f5;
+				color: #fff;
+			}
+		}
+
+		.demo-table-error-row .td {
+			background-color: #ff6600;
+			color: #fff;
+
+			.td_wrap {
+				background-color: #ff6600;
+				color: #fff;
+			}
+		}
+
+		.td.demo-table-info-column {
+			background-color: #2db7f5;
+			color: #fff;
+
+			.td_wrap {
+				background-color: #2db7f5;
+				color: #fff;
+			}
+		}
+
+	
+
+		.table_box {
+				.demo-table-info-cell-name {
+				background-color: #2db7f5;
+				color: #fff;
+			
+				.td_wrap {
+					background-color: #2db7f5;
+					color: #fff;
+				}
+			}
+			.demo-table-info-cell-age {
+				background-color: #ff6600;
+				color: #fff;
+
+				.td_wrap {
+					background-color: #ff6600;
+					color: #fff;
+				}
+
+			}
+
+			.demo-table-info-cell-address {
+				background-color: #187;
+				color: #fff;
+
+				.td_wrap {
+					background-color: #187;
+					color: #fff;
+				}
+			}
+		}
+
+
+		.btn-delete {
+			background: red !important;
+			color: #fff !important;
+		}
+
+
+		.example {
+			.title {
+				line-height: 40px;
+				font-weight: bold;
+			}
 		}
 	}
-
-	//自定义某一行样式
-	.demo-table-info-row .td {
-		background-color: #2db7f5;
-		color: #fff;
-	}
-
-	.demo-table-error-row .td {
-		background-color: #ff6600;
-		color: #fff;
-	}
-
-	.td.demo-table-info-column {
-		background-color: #2db7f5;
-		color: #fff;
-	}
-
-	.demo-table-info-cell-name {
-		background-color: #2db7f5;
-		color: #fff;
-	}
-
-	.demo-table-info-cell-age {
-		background-color: #ff6600;
-		color: #fff;
-	}
-
-	.demo-table-info-cell-address {
-		background-color: #187;
-		color: #fff;
-	}
-
-	.btn-delete {
-		background: red !important;
-		color: #fff !important;
-	}
-	
-	
-	.example{
-		.title {
-			line-height: 40px;
-			font-weight: bold;
-		}
-	}
-		
 </style>
